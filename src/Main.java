fa30b7c1bf0bdf34533719547be3a32837c17513
kayahan <kskrev@gmail.com>
2023-06-26 20:17:12 +0300

    validate input, get methods, optional leave type input

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 import java.io.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Locale;
@@ -30,17 +33,25 @@ public class Main {
         return matcher.matches();
     }
 
+//    public static boolean isDate(String value, Locale locale) {
+//        SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyy", locale);
+//        dateFormat.setLenient(false);
+//        try {
+//            Date date = dateFormat.parse(value);
+//            return true;
+//        } catch (ParseException e) {
+//            return false;
+//        }
+//    }
     public static boolean isDate(String value, Locale locale) {
-        SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyy", locale);
-        dateFormat.setLenient(false);
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd.MM.yyy", locale);
         try {
-            Date date = dateFormat.parse(value);
+            LocalDate date = LocalDate.parse(value, dateFormatter);
             return true;
-        } catch (ParseException e) {
+        } catch (DateTimeParseException e) {
             return false;
         }
     }
-
     public static void writeToFile(ArrayList userInfo) throws IOException {
         FileWriter fileWriter = new FileWriter("DB_Leave_request.txt", true);
         FileWriter fileWriterUid = new FileWriter("UniqueID.txt");
@@ -111,6 +122,62 @@ public class Main {
         return arrayList;
     }
 
+    public static String getLeaveType(Scanner input) {
+        String leaveType;
+        System.out.println("Enter Leave type");
+        System.out.println("1: Paid");
+        System.out.println("2: Unpaid");
+        leaveType = input.next();
+        if (leaveType.equalsIgnoreCase("1")) {
+            leaveType = "Paid";
+        } else if (leaveType.equalsIgnoreCase("2")) {
+            leaveType = "Unpaid";
+        } else {
+            System.out.println("wrong choice!");
+            getLeaveType(input);
+        }
+        return leaveType;
+    }
+
+    public static String getEgn(Scanner input) {
+        String egn;
+        do {
+            System.out.println("Enter your EGN : ");
+            egn = input.next();
+        } while (!isValidEGN(egn));
+        return egn;
+    }
+
+    public static String getStartDate(Scanner input) {
+        String startDate;
+        do {
+            System.out.println("Enter start date (dd.MM.yyyy): ");
+            startDate = input.next();
+        }
+        while (!isDate(startDate, Locale.ENGLISH));
+        return startDate;
+    }
+
+    public static String getEndDate(Scanner input) {
+        String endDate;
+        do {
+            System.out.println("Enter end date (dd.MM.yyyy): ");
+            endDate = input.next();
+        } while (!isDate(endDate, Locale.ENGLISH));
+
+        return endDate;
+    }
+
+    public static String getEmailAddr(Scanner input) {
+        String mail;
+        do {
+            System.out.println("Enter email address: ");
+            mail = input.next();
+        }
+        while (!isValidEmail(mail));
+        return mail;
+    }
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         boolean quit = false;
@@ -122,46 +189,25 @@ public class Main {
                 case 1:
                     //Verifications need to be added!
                     //UID for each request;
+                    //task1
                     ArrayList<String> inputData = new ArrayList<>();
-                    System.out.println("Enter your name: ");
+                    System.out.println("Enter your Name: ");
                     String name = input.next();
                     inputData.add(name);
-                    String mail;
-                    do {
-                        System.out.println("Enter email address: ");
-                        mail = input.next();
-                    }
-                    while (!isValidEmail(mail));
-                    inputData.add(mail);
-                    String egn;
-                    do {
-                        System.out.println("Enter your EGN : ");
-                        egn = input.next();
-                    } while (!isValidEGN(egn));
-                    inputData.add(egn);
-                    String startDate;
-                    do {
-                        System.out.println("Enter start date (dd.MM.yyyy): ");
-                        startDate = input.next();
-                    }
-                    while (!isDate(startDate, Locale.ENGLISH));
-                    inputData.add(startDate);
-                    String endDate;
-                    do {
-                        System.out.println("Enter end date: ");
-                        endDate = input.next();
-                    } while (isDate(endDate, Locale.ENGLISH));
-                    inputData.add(endDate);
-                    System.out.println("Enter Leave type:");
-                    String leaveType = input.next();
-                    inputData.add(leaveType);
+                    System.out.println("Enter your Surname: ");
+                    String surName = input.next();
+                    inputData.add(surName);
+                    inputData.add(getEmailAddr(input));
+                    inputData.add(getEgn(input));
+                    inputData.add(getStartDate(input));
+                    inputData.add(getEndDate(input));
+                    inputData.add(getLeaveType(input));
                     System.out.println(inputData);
                     try {
                         writeToFile(inputData);
                     } catch (IOException ex) {
                         System.out.println(ex);
                     }
-                    //task1
                     break;
                 case 2:
                     //task2
