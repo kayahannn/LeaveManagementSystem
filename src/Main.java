7a085c4721eaf3d95f38c47fe31477af852062d7
kayahan <kskrev@gmail.com>
2023-07-08 16:55:52 +0300

    feature Approve/Reject record

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
 import java.io.*;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
@@ -30,7 +28,7 @@ public class Main {
         return matcher.matches();
     }
 
-    public static boolean isDate(String value, Locale locale) {
+    public static boolean isValidDate(String value, Locale locale) {
         DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy", locale);
         try {
             LocalDate date = LocalDate.parse(value, dateFormatter);
@@ -40,24 +38,22 @@ public class Main {
         }
     }
 
-    public static boolean isDate2BeforeDate1(LocalDate date1, LocalDate date2) {
-        return date2.isBefore(date1);
+    public static boolean isDate2AfterDate1(LocalDate date1, LocalDate date2) {
+
+        return date2.isAfter(date1);
     }
 
     public static void writeToFile(ArrayList userInfo) throws IOException {
         FileWriter fileWriter = new FileWriter("DB_Leave_request.csv", true);
-        FileWriter fileWriterUid = new FileWriter("UniqueID.txt");
         BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-        BufferedWriter bufferedWriter1 = new BufferedWriter(fileWriterUid);
         bufferedWriter.newLine();
+
         for (int i = 0; i < userInfo.size(); i++) {
             bufferedWriter.write(userInfo.get(i).toString() + ",");
         }
-//        bufferedWriter.write(String.valueOf(nextId()) + ",");
+
         bufferedWriter.write("Pending");
         bufferedWriter.close();
-//        bufferedWriter1.write(String.valueOf(nextId()));
-        bufferedWriter1.close();
     }
 
     public static ArrayList<String[]> readFromFile(BufferedReader bufferedReader) throws IOException {
@@ -70,12 +66,12 @@ public class Main {
         return arrayList;
     }
 
-    public static void print(ArrayList<String[]> arrayList) {
-        System.out.printf("|%-10s|%-10s|%-10s|%-20s|%-12s|%-12s|%-12s|%-10s|%n",
-                "UniNumber", "Name", "Surname", "E-mail", "EGN", "Start Date", "End Date", "Leave Type", "Status");
+    public static void printTable(ArrayList<String[]> arrayList) {
+        System.out.printf("|%-10s|%-10s|%-10s|%-20s|%-12s|%-12s|%-12s|%-10s|%-10s|%n",
+                "Number", "Name", "Surname", "E-mail", "EGN", "Start Date", "End Date", "Leave Type", "Status");
         for (int i = 0; i < arrayList.size(); i++) {
             String[] row = arrayList.get(i);
-            System.out.printf("|%-10s|%-10s|%-10s|%-20s|%-12s|%-12s|%-12s|%-10s|%n", row[6], row[0], row[1], row[2], row[3], row[4], row[5], row[7]);
+            System.out.printf("|%-10s|%-10s|%-10s|%-20s|%-12s|%-12s|%-12s|%-10s|%-10s|%n", row[6], row[0], row[1], row[2], row[3], row[4], row[5], row[7], row[8]);
         }
     }
 
@@ -97,70 +93,159 @@ public class Main {
         return arrayList;
     }
 
-    public static String getLeaveType(Scanner input) {
-
+    public static String inputLeaveType(Scanner input) {
         System.out.println("Enter Leave type");
         System.out.println("1: Paid");
         System.out.println("2: Unpaid");
         String leaveType;
         leaveType = input.next();
+
         if (leaveType.equalsIgnoreCase("1")) {
             leaveType = "Paid";
         } else if (leaveType.equalsIgnoreCase("2")) {
             leaveType = "Unpaid";
         } else {
             System.out.println("wrong choice!");
-            leaveType = getLeaveType(input);
+            leaveType = inputLeaveType(input);
         }
+
         return leaveType;
     }
 
-    public static String getEgn(Scanner input) {
+    public static String inputEgn(Scanner input) {
         String egn;
+
         do {
             System.out.println("Enter your EGN : ");
             egn = input.next();
         } while (!isValidEGN(egn));
+
         return egn;
     }
 
-    public static String getStartDate(Scanner input) {
+    public static String inputStartDate(Scanner input) {
         String startDate;
+        boolean isValidDate = false;
+
         do {
             System.out.println("Enter start date (dd.MM.yyyy): ");
             startDate = input.next();
+            isValidDate = isValidDate(startDate, Locale.ENGLISH);
+
+            if (!isValidDate) {
+                System.out.println("Invalid start date format. Please enter dd.MM.yyyy format!");
+            }
         }
-        while (!isDate(startDate, Locale.ENGLISH));
+        while (!isValidDate(startDate, Locale.ENGLISH));
+
         return startDate;
     }
 
-    public static String getEndDate(Scanner input, String startDate) {
+    public static String inputEndDate(Scanner input, String startDate) {
         String endDate;
+        boolean isValidDate = false;
         do {
             System.out.println("Enter end date (dd.MM.yyyy): ");
             endDate = input.next();
-        } while (!isDate(endDate, Locale.ENGLISH) && isDate2BeforeDate1(LocalDate.parse(startDate), LocalDate.parse(endDate)));
+
+            isValidDate = isValidDate(endDate, Locale.ENGLISH);
+            if (!isValidDate) {
+                System.out.println("Invalid input. Please use dd.MM.yyyy format!");
+            }
+            // add check if Date2 is before Date1!
+        } while (!isValidDate(endDate, Locale.ENGLISH) && isDate2AfterDate1(LocalDate.parse(startDate), LocalDate.parse(endDate)));
 
         return endDate;
     }
 
-    public static String getEmailAddr(Scanner input) {
+    public static String inputEmailAddress(Scanner input) {
         String mail;
+
         do {
             System.out.println("Enter email address: ");
             mail = input.next();
+            boolean isValidEmail = isValidEmail(mail);
+
+            if (!isValidEmail) {
+                System.out.println("Invalid input. Please enter correct email address!");
+            }
         }
         while (!isValidEmail(mail));
+
         return mail;
     }
+public static boolean approveLeaveRecord(Scanner input){
+    System.out.println("Approve/Reject Leave: ");
+    System.out.println("1: Approve");
+    System.out.println("2: Reject");
+    String leaveStatus = input.next();
 
-    public static void approveLeave(Scanner input) {
+    if (leaveStatus.equalsIgnoreCase("1")) {
+        return true;
+    } else if (leaveStatus.equalsIgnoreCase("2")) {
+        return false;
+    } else {
+        System.out.println("wrong choice!");
+        approveLeaveRecord(input);
+    }
+    return false;
+}
+    public static void changeLeaveStatus(int number, boolean leaveStatus) throws IOException {
+        List<String[]> lines = new ArrayList<>();
+        String line;
+        boolean recordExist = false;
 
+        try (BufferedReader reader = new BufferedReader(new FileReader("DB_Leave_request.csv"))) {
 
+            while ((line = reader.readLine()) != null) {
+                String[] getData = line.split(",");
+
+                if (getData[6].equalsIgnoreCase(String.valueOf(number))) {
+                    recordExist = true;
+                    if (leaveStatus) {
+                        getData[8] = "Approved";
+                    } else {
+                        getData[8] = "Rejected";
+                    }
+                }
+                lines.add(getData);
+            }
+        }
+        if(!recordExist){
+            System.out.println("Record doesnt exist!");
+            return;
+        }
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("DB_Leave_request.csv"))) {
+            for (int i = 0; i < lines.size(); i++) {
+                String[] lineData = lines.get(i);
+                writer.write(String.join(",", lineData));
+                writer.newLine();
+            }
+        }
+    }
+
+    public static int printTableByNumber() throws FileNotFoundException {
+        int number = 0;
+        try {
+            File file = new File("UniqueID.csv");
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextInt()) {
+                number = scanner.nextInt();
+            }
+            scanner.close();
+            number++;
+            FileWriter writer = new FileWriter("UniqueID.csv");
+            writer.write(String.valueOf(number));
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return number;
     }
 
     public static void printMenu(Scanner input) {
         boolean quit = false;
+        String fileDB = "DB_Leave_request.csv";
         while (!quit) {
             System.out.println("Menu:\n 1. Заяви отпуск\n 2. Виж всички отпуски\n 3. Виж отпуск за служител\n 4. Промени статус на отпуск\n 5. Изход\n");
             System.out.print("Въведи избор: ");
@@ -175,20 +260,22 @@ public class Main {
                         System.out.println("Enter your Name: ");
                         String name = input.next();
                         inputData.add(name);
+
                         System.out.println("Enter your Surname: ");
                         String surName = input.next();
                         inputData.add(surName);
 
-                        inputData.add(getEmailAddr(input));
-
-                        inputData.add(getEgn(input));
-
-                        String startDate = getStartDate(input);
+                        inputData.add(inputEmailAddress(input));
+                        inputData.add(inputEgn(input));
+                        String startDate = inputStartDate(input);
                         inputData.add(startDate);
-                        inputData.add(getEndDate(input, startDate));
-
-                        inputData.add(getLeaveType(input));
-
+                        inputData.add(inputEndDate(input, startDate));
+                        try {
+                            inputData.add(String.valueOf(printTableByNumber()));
+                        } catch (FileNotFoundException e) {
+                            e.printStackTrace();
+                        }
+                        inputData.add(inputLeaveType(input));
                         System.out.println(inputData);
                         try {
                             writeToFile(inputData);
@@ -200,7 +287,7 @@ public class Main {
                         //task2
                         //Print all Leave requests;
                         try {
-                            print(readFromFile(readFile("DB_Leave_request.csv")));
+                            printTable(readFromFile(readFile(fileDB)));
                         } catch (IOException ex) {
                             System.out.println(ex);
                         }
@@ -208,18 +295,29 @@ public class Main {
                     case 3:
                         //task3
                         //Print requests by Name;
-                        System.out.printf("Enter Name: ");
+                        System.out.printf("Enter name: ");
                         name = input.next();
                         try {
-                            print(getLeaveRecordsByName(name, readFile("DB_Leave_request.csv")));
+                            printTable(getLeaveRecordsByName(name, readFile(fileDB)));
                         } catch (IOException ex) {
                         }
                         break;
                     case 4:
                         //task4
-                        //Change status of Leave request by UID;
-                        System.out.println("Enter name: ");
+                        //Change status of Leave request by ID;
+                        try {
+                            printTable(readFromFile(readFile(fileDB)));
+                        } catch (IOException ex) {
+                            System.out.println(ex);
+                        }
+                        System.out.println("Enter number: ");
+                        int num = input.nextInt();
 
+                        try {
+                            changeLeaveStatus(num,approveLeaveRecord(input));
+                        } catch (IOException e) {
+
+                        }
                         break;
                     case 6:
                         quit = true;
